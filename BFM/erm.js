bc4c42756e03a24091b0f9a0e8bce28b57b4525c
AK72KA <akajansivakumaran@gmail.com>
2024-09-07 21:54:19 +0100

    Delete BFM directory

--- BFM/erm.js
+++
@@ -1,8 +0,0 @@
-function openSidePanel() {
-    document.getElementById("mySidePanel").style.width = "250px"; // Adjust width as needed
-  }
-  
-  function closeSidePanel() {
-    document.getElementById("mySidePanel").style.width = "0";
-  }
-  
